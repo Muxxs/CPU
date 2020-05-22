2505f2ff44eedb51e59644f0d96d005a923f2439
Muxxs <muxxsmu@outlook.com>
2020-05-23 01:27:52 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/CPU.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPU.dir/main.c.o"
+  "CPU"
+  "CPU.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CPU.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
