e228cf7f41cd9dc0edf631315589e4d7c2d1f37d
Muxxs <muxxsmu@outlook.com>
2020-05-23 20:23:15 +0800

    基本逻辑结构完成 下一步是进行细化任务

--- main.c
+++ main.c
@@ -1,26 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
+#include <math.h>
+
+
+char Mid[32];
 
 int CountOne=0;  // 程序计数器
 
 // 内存
-char DataMemory[16384][8];  //数据段
-char CodeMemory[16384][8];  //代码段
+int DataMemory[16384][8];  //数据段
+int CodeMemory[16384][8];  //代码段
 
 // 通用寄存器
-char DataRegister[4];  //数据寄存器
-char AddressRegister[4]; //地址寄存器
+int DataRegister[4];  //数据寄存器
+int AddressRegister[4]; //地址寄存器
+int Ir[17]; //指令寄存器
 
-
-
-char ReadFromFile(char CommandContent[512]) //读取指令
-{
-    FILE *fw = fopen("Command.txt", "r");
-    fclose(fw);
-    return "00000001000100000000000000000000";
+// 操作码处理
+int Corporation(char Content[9]){
+    char CharToF[2];
+    int a=0;
+    for (int i=0;i<8;i++){
+        CharToF[0]=Content[i];
+        a+=atof(CharToF)*pow(2,7-i);
+    }
+    return a;
 }
 
+int Subject(char Sub[5]){
+    char CharToF[2];
+    int a=0;
+    for (int i=0;i<4;i++){
+        CharToF[0]=Sub[i];
+        a+=atof(CharToF)*pow(2,3-i);
+    }
+    return a;
+}
+
+
+// 立即数处理
+int Lis(char Li[17]){
+    char CharToF[2];
+    int a=0;
+    for (int i=0;i<16;i++){
+        CharToF[0]=Li[i];
+        a+=atof(CharToF)*pow(2,15-i);
+    }
+    return a;
+}
+
+
+// 根据地址从内存的数据段读取数据
+int GetMemorry(int Address){
+    return DataMemory[Address-16384];
+}
+// 根据地址向内存的数据段写数据
+void WriteMemorry(int Address,int Data){
+    DataMemory[Address-16384]=Data
+}
+
+
+// 程序计数器操作
 int ProgramCount(int type){
     if (type==1){
         CountOne+=4;
@@ -33,12 +75,77 @@ int ProgramCount(int type){
     }
 }
 
+//算术处理方法
+int Calculate(int Op,int Sub1,int Sub2,int Li){
+    if (Op==2){
+        if(Sub2==0){
+            // 寄存器A -> A + 立即数
+            DataRegister[Sub1-1]+=Li;
+        } else{
+            // 寄存器A ->
+        }
+    }
+}
+
+//数据传送方法
+int DataTransform(int Sub1,int Sub2,int Li){
+    if (Sub1>4){
+        WriteMemorry(Sub2-1,DataRegister[Sub1-1]);
+        // 寄存器A 内容 -> 寄存器B 地址 -> 内存
+
+    }else if (Sub2==0){
+        DataRegister[Sub1-1]=Li;
+        // 立即数 -> 寄存器A
+
+    }else{
+        DataRegister[Sub1-1]=GetMemorry(Sub2-1);
+        // 寄存器B 地址 内容 -> 寄存器A
+    }
+}
+
 int main()
 {
-    char *Command;
-    char CommandContent[512] = "Hello";
-    ReadFromFile(CommandContent); //读取指令字符串 获取指针
-    printf("%c\n", *Command);
-    printf("Hello, World!\n");
+    char Command[33]="00000001000101010000000000000001";
+    printf("%s\n",Command); //读取指令字符串 获取指针
+    char Cop[9],SubOne[5],SubTwo[5],Li[17];
+
+    // 二进制转为十进制
+    strncpy(Cop,Command,8);
+    strncpy(SubOne,Command+8,4);
+    strncpy(SubTwo,Command+12,4);\
+    strncpy(Li,Command+16,16);
+    int Coporation=Corporation(Cop);
+    int SubjectOne=Subject(SubOne);
+    int SubjectTwo=Subject(SubTwo);
+    int LiNumber=Lis(Li);
+    printf("Coporation:%d\nSubject One:%d\nSubject Two:%d\nLijishu:%d",Coporation,SubjectOne,SubjectTwo,LiNumber);
+    // 此处获得了操作吗 操作对象 立即数
+
+    if (Coporation==1){
+        //数据传送
+        DataTransform(SubOne,SubTwo,LiNumber);
+
+    }else if (Coporation<=5){
+        //算术
+        Calculate(Coporation,SubOne,SubTwo,LiNumber);
+
+    }else if (Coporation<=8){
+        //逻辑
+
+    } else if (Coporation==9){
+        //比较
+
+    }else if (Coporation==10){
+        //跳转
+
+    }else if (Coporation==11){
+        //输入
+
+    }else if (Coporation==12){
+        //输出
+
+    }
+
+
     return 0;
 }
